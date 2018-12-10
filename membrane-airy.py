666de7dde983a78c321027fef687e0b89b341f74
Krishna Kumar <3963513+kks32@users.noreply.github.com>
2018-12-10 11:52:01 +0100

    :computer: Elastic solution for deflection with Airy stress function

--- membrane-airy.py
+++ membrane-airy.py
@@ -1,3 +1,5 @@
+# A new analytical solution for diaphragm deflection and its application to a Surface-Micromachined
+# pressure sensor https://www.sandia.gov/mstc/_assets/documents/bibliography/5_1New.pdf
 import math
 import csv
 
@@ -6,32 +8,33 @@ def main():
     x0 = 0
     for i in range(0, 101):
         scaling = (1 + (i/100))
-        E = 1E6    # Pa or N/m^2
-        nu = 0.3   #
+        E = 10E6   # Pa or N/m^2
+        nu = 0.43  #
         r = 0.0575 * scaling # m
-        t = 0.0094 # * scaling # m
+        t = 0.00715 * scaling if (0.00715 * scaling) < 0.00748 else 0.00748# m
         mass = 8.5 # kg
         area = math.pi * r * r
-        pressure = 9.814 * (0.02 * 0.20 * 0.15 * 1416.782808612) * pow(scaling, 3) / area #  mass / area #
-        P = 8031.18106 # Pa or N/m^2
-        residual_stress = 80.31 #Pa
+        # pressure = mass * 9.814 / area
+        # pressure = (0.20 * 0.20 * 0.15 * 1416.782808612) * pow(scaling, 3) * 9.814 / area
+        pressure = 8031.18106 * scaling # Pa or N/m^2
 
         alpha = E / (1 - nu)
 
         # ax^3 + bx^2 + cx + d = 0
-        a = 8 * t * alpha / (3 * pow(r, 4))
+        a = 2 * E * t * (1 + nu) * (23 - 9 * nu) / (63 * r * r * (1 - (nu * nu)))
         b = 0
-        c = 4 * t * residual_stress / (r * r)
-        d = -pressure
+        c = 16 * E * pow(t, 3) / (9 * r * r * (1 - (nu * nu)))
+        d = -pressure * r * r / 3
 
-        # Solving for the real values of x in cubic
+        # Generic real value solution of x in a cubic equation
+        # for b = 0
         D0 = -3 * a * c
         D1 = 27 * a * a * d
         f = pow(((D1 + math.sqrt(D1 * D1 - 4 * pow(D0, 3)))/2), (1/3))
-        
+
         # Solving for deflection
         x = -1 / (3 * a) * (f + D0 / f) * 1000 #mm
-        
+        print(x)
         if i == 0:
             x0 = x
 
--- plot.r
+++ plot.r
@@ -2,15 +2,18 @@ library(ggplot2)
 # Set working directory, put the path to the folder containing your files inbetween the inverted commas
 setwd("./")
 #Import your data, save it as a csv in excel with the column headings
-df <- read.csv("effect-radius-deflection.csv", sep=",", header=TRUE)
+df <- read.csv("effect-radius-deflection-airy.csv", sep=",", header=TRUE)
 
 # Line plot with error bar
 p <- ggplot(df, aes(df$parameter)) + 
-     geom_line(aes(y = df$radius, colour = "radius")) + 
-     geom_line(aes(y = df$thickness, colour = "thickness")) +
-     geom_line(aes(y = df$pressure, colour = "pressure")) +
-     scale_colour_manual("Sensitivity", values = c("red", "black", "blue")) +
+     geom_line(aes(y = df$radius, colour = "radius (r)")) + 
+     geom_line(aes(y = df$thickness, colour = "r + thickness (t)")) +
+     geom_line(aes(y = df$all_const_mass, colour = "r + t + P (const mass)")) +
+     geom_line(aes(y = df$pressure, colour = "r + pressure (P)")) +
+     geom_line(aes(y = df$all, colour = "r + t + P")) +
+     scale_colour_manual("Sensitivity", values = c("black", "red", "orange", "green", "blue")) +
      theme_bw() +
+     scale_y_continuous(breaks = seq(-50.0, 1200.0, 50.0)) + 
      # Specify axis labels
      labs(x = "Change in parameter (%)", y = "Change in deflection (%)", title="Effect of change in the radius on the displacement of the pelvic floor") +
      # Font size
@@ -18,5 +21,5 @@ p <- ggplot(df, aes(df$parameter)) +
 
 
 # Save file and scale axis
-ggsave("deflection.pdf",p)
+ggsave("deflection-airy.pdf",p)
 
